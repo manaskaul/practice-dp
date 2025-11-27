2e9845a9dd833eb566c7c3a636e0eedc6adebdb1
Kaul <kaulmanascocid@gmail.com>
2025-11-27 21:55:28 +0530

    Fix off-by-one errors in LengthOfLongestCommonSubstring implementation

--- topic3_longest_common_subsequence/LengthOfLongestCommonSubstring.java
+++ topic3_longest_common_subsequence/LengthOfLongestCommonSubstring.java
@@ -18,14 +18,14 @@ public class LengthOfLongestCommonSubstring {
         for(int j=0; j<m+1; j++) {
             dp[0][j] = 0;
         }
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<n+1; i++) {
             dp[i][0] = 0;
         }
 
         int res = 0;
-        for(int i=1; i<n; i++) {
-            for(int j=1; j<m; j++) {
-                if(x.charAt(i) == y.charAt(j)) {
+        for(int i=1; i<n+1; i++) {
+            for(int j=1; j<m+1; j++) {
+                if(x.charAt(i-1) == y.charAt(j-1)) {
                     dp[i][j] = 1 + dp[i-1][j-1];
                     res = Math.max(res, dp[i][j]);
                 } else {
